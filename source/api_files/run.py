64d03dcf3de80f9848e451b6540a15c3d395f178
LuxxDevs <lkgames256@gmail.com>
2024-01-07 17:09:38 +0100

    asd

--- source/api_files/run.py
+++ source/api_files/run.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+import aiohttp
+import discord
+from discord import Webhook
 from flask import Flask, jsonify, request, render_template
 import json
 from rich.console import Console
@@ -6,6 +11,26 @@ from source import tools
 
 console = Console()
 app = Flask(__name__, template_folder='website')
+
+
+async def send_global_broadcast(message: str):
+    webhook_session = aiohttp.ClientSession()
+    from source.tools import get_column
+    for url in await get_column("../world.db", "world_chats", "webhook_url"):
+        try:
+            webhook = Webhook.from_url(str(url), session=webhook_session)
+            e = discord.Embed(
+                title="🚀 BROADCAST 🚀",
+                description=f"{message}",
+                timestamp=datetime.now(),
+                color=discord.Colour.red()
+            )
+            e.set_footer(text=" ✅ Dies ist eine offizielle Nachricht ✅")
+            await webhook.send(embed=e, avatar_url="https://i.ibb.co/D96qZq7/KH75-World-Chat-2.png")
+        except discord.NotFound as e:
+            print(
+                "Es ist ein Fehler aufgetreten. Möglicherweise gibt es einen Webhook in der Datenbank, der nicht vorhanden ist.")
+            pass
 
 
 @app.route('/')
@@ -30,5 +55,6 @@ async def handle_input():
 
 def start_server(host: str = "172.17.0.3", port: int = 8001, debug: bool = True):
     app.run(debug=debug, port=port, host=host)
+
 
 start_server()
